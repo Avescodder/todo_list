89da94f998114bba5edf43ee0a3800a5446dc03c
Vsevolod Morozov <morozovvsevolod15@gmail.com>
2024-12-05 21:30:02 +0000

    kuber

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -155,12 +155,25 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.MyUser'
 
 INTERNAL_IPS = [os.getenv("INTERNAL_IPS")]
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+
+if DEBUG:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379",
+        }
     }
-}
+else:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379",
+            # 'OPTIONS': {
+            #     'CLIENT_CLASS':'django_redis.client.DefaultClient',
+            #     'PASSWORD': os.getenv("REDIS_PASSWORD"),
+            # }
+        }
+    }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
